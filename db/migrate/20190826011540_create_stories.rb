fd232166820e2a381208818d6afdcf5808e2cfcb
Jessi Calicotte <orgkleen@gmail.com>
2019-08-26 01:39:52 +0000

    fixing migration

--- db/migrate/20190826011540_create_stories.rb
+++ db/migrate/20190826011540_create_stories.rb
@@ -2,7 +2,7 @@ class CreateStories < ActiveRecord::Migration[5.2]
   def change
     create_table :stories do |t|
       t.string :story
-      t.string  :survivor
+      t.string :survivor
     end
   end
 end
